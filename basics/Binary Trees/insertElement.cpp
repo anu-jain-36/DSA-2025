37730c724a590b3b481e3d14f32d8b9d665f12bc
unknown <realanujain@gmail.com>
2024-12-28 22:04:34 -0600

    add element search

--- basics/Binary Trees/insertElement.cpp
+++ basics/Binary Trees/insertElement.cpp
@@ -47,6 +47,21 @@ void inorder(Node* root)
   
 }
 
+bool search(Node* root, int item)
+{
+    bool found;
+    if (root == nullptr)
+        return false;
+    else if(root->key == item)
+       return true;
+    else if(root->key < item)
+     found = search(root->right, item);
+    else 
+     found= search(root->left, item);
+
+    return found;
+}
+
 int main()
 {
     Node* root = new Node(10);
@@ -57,6 +72,9 @@ int main()
     root= insert (root,50);
     root= insert (root,8);
     root= insert (root,40);
+   
+    bool found= search(root, 100);
     inorder(root);
+    cout<<"element found is"<< found;
     return 0;
 }
